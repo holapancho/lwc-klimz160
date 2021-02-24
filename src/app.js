cd5659eb1ff7e0c7ea9be57072b9467447ebda2f
Francisco Rojas <francisco.rojas.morales@gmail.com>
2021-02-24 12:45:46 +0000

    add for each

--- src/app.js
+++ src/app.js
@@ -1,27 +1,6 @@
-import { LightningElement } from "lwc";
+import { track, LightningElement } from "lwc";
 
 export default class App extends LightningElement {
-  title = "Welcome to Lightning Web Components!";
-
-  showFeatures = true;
-
-  /**
-   * Getter for the features property
-   */
-  get features() {
-    return [
-      {
-        label: "Learn in the browser.",
-        icon: "utility:edit",
-      },
-      {
-        label: "View changes to code instantly with Live Compilation.",
-        icon: "utility:refresh",
-      },
-      {
-        label: "Style your components with SLDS.",
-        icon: "utility:brush",
-      },
-    ];
-  }
+  
+  @track tabs = [{ id: 1, name: 'Tab 1' }];
 }
