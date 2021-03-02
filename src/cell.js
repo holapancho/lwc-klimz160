c1b9e642b76bc7c4723c19ab918a0a5074bf5932
Francisco Rojas <francisco.rojas.morales@gmail.com>
2021-03-02 01:44:36 +0000

    propagate changes in repeated columns

--- src/cell.js
+++ src/cell.js
@@ -3,22 +3,27 @@ import { api, track, LightningElement } from "lwc";
 export default class Cell extends LightningElement {
   @api record;
   @api column;
-  @track result;
+  @track field;
 
   connectedCallback(){
+    this.field = this.result;
+  }
+
+  get result(){
     if(this.record && this.column){
-      let result = this.record.fields.find( f => f.columnId === this.column.columnId);
-      if(result){
-        this.result = this.unproxy(result);
+      let field = this.record.fields.find( f => f.columnId === this.column.columnId);
+      if(field){
+        return this.unproxy(field);
       }
     }
+    return {value:''}; //?
   }
 
   itemChanged(event){
-    this.result.value = event.target.value;
+    this.field.value = event.target.value;
     const cellChangeEvent = new CustomEvent('cellchange', {
         detail: {
-          result: this.result,
+          result: this.field,
           record: this.record
       },
 
